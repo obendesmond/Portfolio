0f33e5344291c43ac491649a304e5d02fd8e125c
obendesmond <obendesmond2@gmail.com>
2022-04-14 13:46:57 +0100

    initialization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // images: {
+  //   domains: ["localhost"],
+  //   // next line is not required
+  //   // path: "http://localhost:3000/images",
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        myGreen: "#009743",
+        myYellow: "#ffcb0a",
+        myDark: "#0F172A",
+      },
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+  },
+  plugins: [],
+};
