ab4743f94ebe281085ba3f50fc337583c947b8ff
obendesmond <obendesmond2@gmail.com>
2022-04-19 12:32:37 +0100

    try 100px font size

--- src/components/ContactLinkIcon.js
+++ src/components/ContactLinkIcon.js
@@ -4,7 +4,7 @@ export default function ContactLinkIcon({ Icon, title, tooltipPosition }) {
   return (
     <CustomTooltip title={title} tooltipPosition={tooltipPosition}>
       <Icon
-        fontSize="large"
+        fontSize="100px"
         className="bg-opacity-80 backdrop-blur-[1px] text-white p-[5px] text-5xl bg-myGreen rounded-full hover:bg-myYellow hover:text-myDark"
       />
     </CustomTooltip>
