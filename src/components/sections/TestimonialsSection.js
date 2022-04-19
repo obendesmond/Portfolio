df469d611206f20c1a65814ab9fac94200fa966c
obendesmond <obendesmond2@gmail.com>
2022-04-19 16:01:39 +0100

    add quote-start

--- src/components/sections/TestimonialsSection.js
+++ src/components/sections/TestimonialsSection.js
@@ -22,12 +22,52 @@ export default function TestimonialsSection() {
       </div>
 
       <div className="pt-10">
-        <a
-          href="#more-education"
-          className="text-gray-400 font-extrabold text-2xl"
-        >
-          See More Certs...
-        </a>
+        <div className="relative container mx-auto bg-myDark rounded-b-3xl px-20 py-10">
+          <div
+            className="absolute top-5 left-5 text-8xl text-gray-400"
+            aria-hidden="true"
+          >
+            &#8220;
+          </div>
+          <div className="mb-10">
+            <BodyText>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui
+              nihil corrupti eius sapiente impedit temporibus deserunt
+              praesentium? Delectus nesciunt, ducimus illum libero porro a
+              earum, odit soluta ut maiores ratione?
+            </BodyText>
+          </div>
+          <p className="text-gray-400 text-xl text-left font-bold">@Perick</p>
+          <div className=" -mb-[70px] items-center justify-center w-full flex flex-row mx-auto space-x-10">
+            <div>
+              <Image
+                width={50}
+                height={50}
+                className="object-cover rounded-full grayscale"
+                alt="perick"
+                src="/assets/testimonials/Perick.webp"
+              />
+            </div>
+            <div>
+              <Image
+                width={60}
+                height={60}
+                className="object-cover rounded-full"
+                alt="perick"
+                src="/assets/testimonials/marc.jpeg"
+              />
+            </div>
+            <div>
+              <Image
+                width={50}
+                height={50}
+                className="object-cover rounded-full grayscale"
+                alt="perick"
+                src="/assets/testimonials/john.jpg"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
