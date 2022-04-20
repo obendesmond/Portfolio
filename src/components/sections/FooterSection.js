f2ce33de216d6f69107b7c8073a4fb9f56c91797
obendesmond <obendesmond2@gmail.com>
2022-04-20 16:01:39 +0100

    fix bottom footer to get year automatically

--- src/components/sections/FooterSection.js
+++ src/components/sections/FooterSection.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function FooterSection() {
+  const year = new Date().getFullYear();
+
   return (
     <div className="bg-myBorder p-5 text-gray-400">
       <div className="container text-center relative mx-auto">
         <p>
-          &copy; 2022. Made with 💜 by{" "}
+          &copy; {year}. Made with 💜 by{" "}
           <a href="#hero" className="text-myYellow">
             Desmond Oben
           </a>
